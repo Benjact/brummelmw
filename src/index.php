512220bc3783b08fb6764019088dac27a66c9a7e
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-08-13 18:08:05 +0200

    Debug acceso bot telegram

--- src/index.php
+++ src/index.php
@@ -10,21 +10,16 @@ use BrummelMW\core\BotHTML;
 use BrummelMW\response\Response;
 use BrummelMW\response\ResponseHTML;
 
-if ($_SERVER["HTTP_HOST"] == RUTA_WEB) {
-    /*if (!isset($_GET["instruccion"])) $_GET["instruccion"] = "";
+$update = json_decode(file_get_contents("php://input"), true);
+if (is_null($update)) {
+    if (!isset($_GET["instruccion"])) $_GET["instruccion"] = "";
 
     $bot = new BotHTML("/" . $_GET["instruccion"]);
     $response = new ResponseHTML();
-*/
-    $update = [];
-    $update["message"]["chat"]["id"] = "411317956";
-    $update["message"]["chat"]["type"] = "private";
-    $update["message"]["text"] = "/ayuda";
 } else {
-    $update = json_decode(file_get_contents("php://input"), true);
+    $bot = new Bot(TOKEN, RUTA_API, $update);
+    $response = new Response($bot);
 }
-$bot = new Bot(TOKEN, RUTA_API, $update);
-$response = new Response($bot);
 
 $acciones = new AccionesGeneral($bot->mensaje());
 $response->devolverMensaje($acciones->retorno());
