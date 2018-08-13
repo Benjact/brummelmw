e3fe22f59c7d666bab4394291b5eb84d7b372d6d
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-08-13 17:05:55 +0200

    Personajes y ayuda

--- src/acciones/AccionesGeneral.php
+++ src/acciones/AccionesGeneral.php
@@ -11,8 +11,13 @@ class AccionesGeneral
     {
         $instruccion_partida = explode(" ", $instruccion);
         $primera_palabra = $instruccion_partida[0];
-        if ($primera_palabra == "/ayuda") {
-            $this->instruccion = new Ayuda();
+
+        if ($primera_palabra == "/ayuda" || $primera_palabra == "/help") {
+            if (isset($instruccion_partida[1])) {
+                $this->instruccion = new Ayuda($instruccion_partida[1]);
+            } else {
+                $this->instruccion = new Ayuda();
+            }
 
         } elseif ($primera_palabra == "/personajes") {
             if (isset($instruccion_partida[1])) {
@@ -20,6 +25,8 @@ class AccionesGeneral
             } else {
                 $this->instruccion = new Personajes();
             }
+        } elseif (in_array($primera_palabra, (new Personajes())->personajes())) {
+            $this->instruccion = new Personajes($primera_palabra);
 
         } elseif ($primera_palabra == "/excel") {
             $this->instruccion = new Excel();
--- src/acciones/Ayuda.php
+++ src/acciones/Ayuda.php
@@ -1,10 +1,23 @@
 <?php
 namespace BrummelMW\acciones;
 
+use BrummelMW\acciones\swgoh\Personajes;
+
 class Ayuda extends AccionBasica implements iAcciones
 {
+    private $parametro = "";
+
+    public function __construct(string $parametro = "")
+    {
+        $this->parametro = $parametro;
+    }
+
     public function retorno(): string
     {
-        return "Estoy en ello";
+        if ($this->parametro == "personajes") {
+            return "Personajes posibles:<br>" . implode("<br>", (new Personajes())->personajes());
+        } else {
+            return "Estoy en ello";
+        }
     }
 }
