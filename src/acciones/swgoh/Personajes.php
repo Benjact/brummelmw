abd5de7652c73dec4b553457885c2dfe62b3acef
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-08-13 18:38:34 +0200

    Acceso bot telegram

--- src/acciones/swgoh/Personajes.php
+++ src/acciones/swgoh/Personajes.php
@@ -17,7 +17,7 @@ class Personajes extends SWGOH
         $array_personajes = $this->personajes();
 
         if ($this->personaje == "") {
-            return "Personajes posibles:";//<br>" . implode("<br>", $array_personajes);
+            return "Personajes posibles:<br>" . implode("<br>", $array_personajes);
         } else {
             if (in_array($this->personaje, $array_personajes)) {
                 return "veamos {$this->personaje}";
