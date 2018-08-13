c9cb1e64e7e56844c4497af6c27a93c4e1fd98e5
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-08-13 18:48:43 +0200

    refactor AccionesGeneral

--- src/response/ResponseHTML.php
+++ src/response/ResponseHTML.php
@@ -3,11 +3,7 @@ namespace BrummelMW\response;
 
 class ResponseHTML
 {
-    public function __construct()
-    {
-    }
-
-    public function devolverMensaje(string $mensaje)
+    public function devolverMensaje($mensaje)
     {
         echo is_array($mensaje) ? implode("<br>", $mensaje) : $mensaje;
     }
