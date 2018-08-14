921083af79de9027a61f9475c6aa6e665868f032
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-08-14 09:03:27 +0200

    Usar nombre personaje directamente

--- src/acciones/Ayuda.php
+++ src/acciones/Ayuda.php
@@ -27,11 +27,11 @@ class Ayuda extends AccionBasica implements iAcciones
                 //"/ayuda jugadores: devolverá un listado de jugadores del gremio registrados en swgoh",
                 "/personajes: devolverá un listado de personajes posibles",
                 "/personajes [NOMBRE_PERSONAJE]: devolverá información de dicho personaje",
+                "/[NOMBRE_PERSONAJE]: lo mismo que el anterior",
                 "/personajes [NOMBRE_PERSONAJE] [NUMERO_ESTRELLAS]: indicará cuantos hay en el gremio con esas o más estrellas y quién los tiene",
+                "/[NOMBRE_PERSONAJE] [NUMERO_ESTRELLAS]: lo mismo que el anterior",
                 "/brummel: indicaciones indispensables para el gremio",
                 "/autosith: posibles equipos para pasarse la raid sith en auto",
-                //"/[NOMBRE_PERSONAJE]: lo mismo que el anterior",
-                //"/[NOMBRE_PERSONAJE] [NUMERO_ESTRELLAS]: lo mismo que el anterior",
                 //"/jugadores: devolverá un listado de jugadores del gremio registrados en swgoh",
             ];
         }
