65a26906e180b9bfe27f4803aa72b06831ad4f12
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-09-04 20:50:46 +0200

    Pruebas Excel

--- src/acciones/origenExcel/BT.php
+++ src/acciones/origenExcel/BT.php
@@ -29,7 +29,16 @@ class BT extends Excel
         if (empty($values)) {
             return parent::comprobarValores($values);
         } else {
-            return $values;
+            $array_pelotones = [];
+            $filas = ["norte" => 16, "centro" => 34, "sur" => 52];
+            $columnas = [4, 8, 12, 16, 20, 24];
+            foreach ($filas as $posicion_mapa => $fila) {
+                $array_pelotones[] = $posicion_mapa;
+                foreach ($columnas as $n_peloton => $columna) {
+                    $array_pelotones[] = "PELOTON {$n_peloton} :".($values[$fila][$columna] == "" ? "SI" : "NO");
+                }
+            }
+            return $array_pelotones;
         }
     }
 }
