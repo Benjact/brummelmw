095ad2da2334e4373298bb7978f6ab94a28b0e87
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-09-13 17:32:11 +0200

    Errores Ruta

--- src/acciones/origenSwgoh/SwgohGuildUnits.php
+++ src/acciones/origenSwgoh/SwgohGuildUnits.php
@@ -1,18 +1,16 @@
 <?php
 namespace BrummelMW\acciones\origenSwgoh;
 
-use BrummelMW\acciones\AccionBasica;
-use BrummelMW\acciones\ExcepcionAccion;
 use BrummelMW\acciones\ExcepcionRuta;
-use BrummelMW\acciones\iAcciones;
 
 class SwgohGuildUnits implements iSWGOH
 {
     public static function recuperarJSON(): array
     {
         $ruta = "https://swgoh.gg/api/guilds/" . ID_GREMIO . "/units/";
-        $array_retorno = json_decode(file_get_contents($ruta), true);
-        if ($array_retorno == null) {
+        if (file_exists($ruta)) {
+            $array_retorno = json_decode(file_get_contents($ruta), true);
+        } else {
             throw new ExcepcionRuta($ruta);
         }
         return $array_retorno;
--- src/acciones/origenSwgoh/SwgohShips.php
+++ src/acciones/origenSwgoh/SwgohShips.php
@@ -1,16 +1,16 @@
 <?php
 namespace BrummelMW\acciones\origenSwgoh;
 
-use BrummelMW\acciones\AccionBasica;
-use BrummelMW\acciones\iAcciones;
+use BrummelMW\acciones\ExcepcionRuta;
 
 class SwgohShips implements iSWGOH
 {
     public static function recuperarJSON(): array
     {
         $ruta = "https://swgoh.gg/api/ships/?format=json";
-        $array_retorno = json_decode(file_get_contents($ruta), true);
-        if ($array_retorno == null) {
+        if (file_exists($ruta)) {
+            $array_retorno = json_decode(file_get_contents($ruta), true);
+        } else {
             throw new ExcepcionRuta($ruta);
         }
         return $array_retorno;
