60dfdcf4deb70cbc9654f166b236b6c524a14bc6
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-09-17 19:35:52 +0200

    Nuevo JSON datos gremio

--- src/acciones/origenSwgoh/Personajes.php
+++ src/acciones/origenSwgoh/Personajes.php
@@ -50,7 +50,7 @@ class Personajes extends AccionCompuesta
             $coincidencia = str_replace("%", "", $this->personaje);
             $array_personajes_coincidentes = Utils::filtrar($array_personajes, $coincidencia);
             if (count($array_personajes_coincidentes) == 1) {
-                $this->setPersonaje(str_replace(BOLD, "", str_replace(BOLD_CERRAR, "", $array_personajes_coincidentes[1])));
+                $this->setPersonaje(str_replace(BOLD, "", str_replace(BOLD_CERRAR, "", $array_personajes_coincidentes[0])));
                 return $this->retorno($id_chat);
             } else {
                 asort($array_personajes_coincidentes);
--- src/core/Utils.php
+++ src/core/Utils.php
@@ -5,13 +5,13 @@ class Utils
 {
     public static function filtrar(array $array_filtrar, string $filtro): array
     {
-        return array_filter(array_map(function ($personaje) use ($filtro) {
+        return array_values(array_filter(array_map(function ($personaje) use ($filtro) {
             $strpos = strpos(mb_strtoupper($personaje), mb_strtoupper($filtro));
             if ($strpos !== false) {
                 return str_replace($filtro, BOLD.$filtro.BOLD_CERRAR, $personaje);
             }
             return "";
-        }, $array_filtrar));
+        }, $array_filtrar)));
     }
 
     public static function comprobarExisteRuta(string $ruta): bool
