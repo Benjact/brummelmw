71d2e43966b8bca9cd6708661f8c18aa3ae0e03d
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-09-17 19:13:19 +0200

    Nuevo JSON datos gremio

--- src/acciones/origenSwgoh/SwgohGuildUnits.php
+++ src/acciones/origenSwgoh/SwgohGuildUnits.php
@@ -7,7 +7,7 @@ class SwgohGuildUnits implements iSWGOH
 {
     public static function recuperarJSON(): array
     {
-        $ruta = "https://swgoh.gg/api/guild/" . ID_GREMIO ;
+        $ruta = "https://swgoh.gg/api/guild/" . ID_GREMIO . "/";
         if (file_exists($ruta)) {
             $array_retorno = json_decode(file_get_contents($ruta), true);
         } else {
