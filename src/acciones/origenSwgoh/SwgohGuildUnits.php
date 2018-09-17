6b163f5cfeab7506f21148cb0bfdfb3a53ca0fd8
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-09-17 19:16:50 +0200

    Nuevo JSON datos gremio

--- src/acciones/origenSwgoh/SwgohGuildUnits.php
+++ src/acciones/origenSwgoh/SwgohGuildUnits.php
@@ -10,11 +10,11 @@ class SwgohGuildUnits implements iSWGOH
         $ruta = "https://swgoh.gg/api/guild/" . ID_GREMIO . "/";
         $file_headers = @get_headers($ruta);
         if ($file_headers[0] == 'HTTP/1.0 404 Not Found') {
-            throw new ExcepcionRuta($ruta);
+            throw new ExcepcionRuta("1 ".$ruta);
         }
 
         if ($file_headers[0] == 'HTTP/1.0 302 Found' && $file_headers[7] == 'HTTP/1.0 404 Not Found') {
-            throw new ExcepcionRuta($ruta);
+            throw new ExcepcionRuta("2 ".$ruta);
         }
 
         return json_decode(file_get_contents($ruta), true);
