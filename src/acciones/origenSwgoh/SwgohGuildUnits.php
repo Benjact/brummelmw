6e40d99255c0fe58767134f6bdd9f499d883fe38
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-11-03 12:08:44 +0100

    Acivo Personajes demasiado grande

--- src/acciones/origenSwgoh/SwgohGuildUnits.php
+++ src/acciones/origenSwgoh/SwgohGuildUnits.php
@@ -3,6 +3,9 @@ namespace BrummelMW\acciones\origenSwgoh;
 
 use BrummelMW\acciones\ExcepcionRuta;
 use BrummelMW\core\Utils;
+use Exception;
+use NoRewindIterator;
+use SplFileObject;
 
 class SwgohGuildUnits implements iSWGOH
 {
@@ -10,15 +13,70 @@ class SwgohGuildUnits implements iSWGOH
 
     public static function recuperarJSON(): array
     {
-        ini_set('memory_limit','512MB');
-
         $ruta = "https://swgoh.gg/api/guild/" . ID_GREMIO . "/";
-        echo $ruta;
         if (!Utils::comprobarExisteRuta($ruta)) {
             throw new ExcepcionRuta($ruta);
         }
 
-        echo file_get_contents($ruta);
+        $largefile = new BigFile($ruta);
+        $iterator = $largefile->iterate("Text"); // Text or Binary based on your file type
+        foreach ($iterator as $line) {
+            echo $line;
+        }
         return json_decode(file_get_contents($ruta), true);
+    }
+}
+
+class BigFile
+{
+    protected $file;
+
+    public function __construct($filename, $mode = "r")
+    {
+        if (!file_exists($filename)) {
+
+            throw new Exception("File not found");
+
+        }
+
+        $this->file = new SplFileObject($filename, $mode);
+    }
+
+    protected function iterateText()
+    {
+        $count = 0;
+
+        while (!$this->file->eof()) {
+
+            yield $this->file->fgets();
+
+            $count++;
+        }
+        return $count;
+    }
+
+    protected function iterateBinary($bytes)
+    {
+        $count = 0;
+
+        while (!$this->file->eof()) {
+
+            yield $this->file->fread($bytes);
+
+            $count++;
+        }
+    }
+
+    public function iterate($type = "Text", $bytes = NULL)
+    {
+        if ($type == "Text") {
+
+            return new NoRewindIterator($this->iterateText());
+
+        } else {
+
+            return new NoRewindIterator($this->iterateBinary($bytes));
+        }
+
     }
 }
