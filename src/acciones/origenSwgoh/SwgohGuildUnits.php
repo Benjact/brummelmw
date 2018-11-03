b2d74f0e27e30ce23c7b393a4b74efcaf4c18a31
benjact@gmail.com <benjamin.colomina@nodoambar.com>
2018-11-03 10:56:04 +0100

    Motes de personajes

--- src/acciones/origenSwgoh/SwgohGuildUnits.php
+++ src/acciones/origenSwgoh/SwgohGuildUnits.php
@@ -13,10 +13,12 @@ class SwgohGuildUnits implements iSWGOH
         ini_set('memory_limit','256MB');
 
         $ruta = "https://swgoh.gg/api/guild/" . ID_GREMIO . "/";
+        echo $ruta;
         if (!Utils::comprobarExisteRuta($ruta)) {
             throw new ExcepcionRuta($ruta);
         }
 
+        echo file_get_contents($ruta);
         return json_decode(file_get_contents($ruta), true);
     }
 }
